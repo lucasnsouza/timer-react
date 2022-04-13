f408752bdc41fb7cfe84c5d877402b1ffb4b08f6
lucasnsouza <lucasnstj@gmail.com>
2022-04-13 14:59:06 -0300

    trabalhando com state para pegar e alterar value dos inputs do formulario

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ import style from './Button.module.scss';
 class Button extends React.Component {
     render() {
         return (
-            <button className={style.botao} type="submit">
+            <button className={style.botao}>
                 {/* indica que esse component espera um dom children */}
                 {this.props.children}
             </button>
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
-import style from './Form.module.scss';
+import style from "./Form.module.scss";
 
-class Form extends React.Component {
-    render() {
-        return (
-            <form className={style.novaTarefa}>
-                <div className={style.inputContainer}>
-                    <label htmlFor="tarefa">Adicione um novo estudo</label>
-                    <input 
-                    type="text"
-                    name="tarefa"
-                    id="tarefa"
-                    placeholder="O que você quer estudar?" 
-                    required
-                    />
-                </div>
-                <div className={style.inputContainer}>
-                    <label htmlFor="tempo">Tempo</label>
-                    <input 
-                    type="time"
-                    step="1"
-                    name="tempo"
-                    id="tempo"
-                    min="00:00:00"
-                    max="01:30:00"
-                    required 
-                    />
-                </div>
-                <Button>
-                    Adicionar
-                </Button>
-            </form>
-        )
-    }
+function Form() {
+  /*
+  usando state do react para controlar os values dos inputs de tarefa e tempo
+  dessa forma podemos pegar esses valores e passar para a lista
+  */
+  const [estadoDoForm, setEstadoDoForm] = useState({tarefa: "", tempo: "00:00"});
+
+  function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+      //pegamos o evento como parametro
+      //e agora vamos impedir que o form siga o padrão e dê refresh na página
+      evento.preventDefault();
+      console.log(estadoDoForm);
+  }
+
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          value={estadoDoForm.tarefa}
+          onChange={evento => setEstadoDoForm({...estadoDoForm, tarefa: evento.target.value})}
+          id="tarefa"
+          placeholder="O que você quer estudar?"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          value={estadoDoForm.tempo}
+          onChange={evento => setEstadoDoForm({...estadoDoForm, tempo: evento.target.value})}
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button>Adicionar</Button>
+    </form>
+  );
 }
 
 export default Form;
