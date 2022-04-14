3e0434e0904a8e846d8cd481c6a6a76bbf3f348d
lucasnsouza <lucasnstj@gmail.com>
2022-04-14 15:52:47 -0300

    usamos useEffect para monitorar alterações no estado do item selecionado e fazer a conversão

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefa } from "../../types/tarefa";
 import Button from "../Button";
@@ -6,28 +6,31 @@ import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
 interface Props {
-    selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined;
 }
 
-function Timer({ selecionado } : Props) {
-    //vamos criar um estado para tempo
-    //já que vamos usar esse valor no nosso cronometro
-    const [tempo, setTempo] = useState<number>();
+function Timer({ selecionado }: Props) {
+  //vamos criar um estado para tempo
+  //já que vamos usar esse valor no nosso cronometro
+  const [tempo, setTempo] = useState<number>();
+  //permite monitorar se a prop selecionado mudou ou foi selecionado outro item
+    useEffect(() => {
+    //daí com o item selecionado podemos verificar se ele existe e converter para segundos
     if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
+      setTempo(tempoParaSegundos(selecionado.tempo));
     }
-    return (
-        <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um card e incie o cronômetro</p>
-            Tempo: {tempo}
-            <div className={style.relogioWrapper}>
-            <Clock/>
-            </div>
-            <Button>
-                Começar
-            </Button>
-        </div>
-    )
+  }, [selecionado]);
+
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e incie o cronômetro</p>
+      Tempo: {tempo}
+      <div className={style.relogioWrapper}>
+        <Clock />
+      </div>
+      <Button>Começar</Button>
+    </div>
+  );
 }
 
 export default Timer;
